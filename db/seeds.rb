3d7bc3f074481ad9ed552c9684414d0661a6c4e1
Lena Namiki <lena.namiki@gmail.com>
2021-05-20 14:33:55 +0900

    Added movies to database, added lists controller and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts 'Fetching url data...'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+movies = JSON.parse(URI.open(url).read)['results']
+
+puts 'Creating movie database...'
+
+movies.each do |m|
+  Movie.create!(
+    title: m['title'],
+    overview: m['overview'],
+    poster_url: m['poster_path'],
+    rating: m['vote_average']
+  )
+end
+
+puts 'All done!'
